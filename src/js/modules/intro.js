932ca2ce69cc5f1b5f83ba5982e6f9a1a9b2a156
ivansukhoparov <rbcdaaa@gmail.com>
2023-03-07 02:40:18 +0400

    create: making layout for big blocks

--- src/js/modules/intro.js
+++ src/js/modules/intro.js
@@ -77,6 +77,7 @@ function load() {
 
 function loadPage() {
     document.querySelector('body').classList.add('page');
+    document.querySelector('body').classList.remove('intro');
     cmdLine.classList.add('hidden');
     setTimeout(() => {
         intro.remove();
@@ -87,7 +88,11 @@ function loadPage() {
 function showPage(params) {
     const sections = document.querySelectorAll('section');
     const menu = document.querySelector('.nav');
+    const social = document.querySelector('.social');
+    const email = document.querySelector('.page__email');
 
+    email.classList.remove('hidden');
+    social.classList.remove('hidden');
     menu.classList.remove('hidden');
     sections.forEach(element => {
         element.classList.remove('hidden');
